f8a2c2c3159a863110d67ad62561dc1b10673177
Raphael Carlos Rego <raphaelcarlosr@gmail.com>
2024-03-12 21:35:09 +0000

    Refactor code to improve performance and readability

--- sdk/lib/builder.ywt.sh
+++ sdk/lib/builder.ywt.sh
@@ -3,9 +3,10 @@
 builder() {
     local CONTEXT="[BUILDER] " && readonly CONTEXT
     local CONFIG_EXPIRES_AT="${YWT_CONFIG_BUILDER_EXPIRES_AT:-"31/12/2999"}"
-    local DIST=${2:-"${YWT_CONFIG_BUILDER_DIST:-"$(jq -r .path.dist <<<"$YWT_CONFIG")"}"}
-    local SRC=${3:-"${YWT_CONFIG_BUILDER_SRC:-"$(jq -r .path.src <<<"$YWT_CONFIG")"}"}
-    local BIN=$(jq -r .path.bin <<<"$YWT_CONFIG")
+    local DIST=${2:-"${YWT_CONFIG_BUILDER_DIST:-"$(jq -r .path.dist <<<"$YWT_CONFIG")"}"} && readonly DIST
+    local SRC=${3:-"${YWT_CONFIG_BUILDER_SRC:-"$(jq -r .path.src <<<"$YWT_CONFIG")"}"} && readonly SRC
+    local SDK=$(jq -r .path.sdk <<<"$YWT_CONFIG") && SDK=$(realpath -- "$SDK") && readonly SDK
+    local BIN=$(jq -r .path.bin <<<"$YWT_CONFIG") && readonly BIN
     _prepare() {
         [[ ! -d "$DIST" ]] && mkdir -p "${DIST}"
         rm -fR "${DIST:?}"/*
@@ -29,57 +30,70 @@ builder() {
         local TARGET=${1:?}
         local EXPIRES_AT="${2:-$CONFIG_EXPIRES_AT}"
         local FILENAME && FILENAME=$(basename -- "$TARGET") && FILENAME="${FILENAME%.*}"
-        local FILE="${DIST}/${FILENAME}.sh"
-        [[ ! -f "$FILE" ]] && echo "$FILE is not a valid file" | logger error && return 0
-        local STAT_FILE="${DIST}/${FILENAME}.stat"
-        [[ -f "$STAT_FILE" ]] && rm -f "$STAT_FILE"
-        touch "$STAT_FILE"
-        {
-            local IS_BINARY=false
-            LC_ALL=C grep -a '[^[:print:][:space:]]' "$FILE" >/dev/null && IS_BINARY=true
-            echo "{"
-            echo "  \"file\": \"$FILE\","
-            echo "  \"expires_at\": \"$EXPIRES_AT\","
-            echo "  \"size\": \"$(du -h "$FILE" | awk '{print $1}')\","
-            echo "  \"md5\": \"$(md5sum "$FILE" | awk '{print $1}')\","
-            echo "  \"sha1\": \"$(sha1sum "$FILE" | awk '{print $1}')\","
-            echo "  \"sha256\": \"$(sha256sum "$FILE" | awk '{print $1}')\","
-            echo "  \"sha512\": \"$(sha512sum "$FILE" | awk '{print $1}')\"",
-            echo "  \"created_at\": \"$(date -Iseconds)\"",
-            echo "  \"binary\": $IS_BINARY"
-            echo "}"
-        } >>"$STAT_FILE"
-        echo "Stat file created at $STAT_FILE" | logger success
-        jq -Cc . <"$STAT_FILE" | logger verbose
+        local IS_BINARY=false
+        LC_ALL=C grep -a '[^[:print:][:space:]]' "$TARGET" >/dev/null && IS_BINARY=true
+        echo "{"
+        echo "  \"file\": \"$TARGET\","
+        echo "  \"expires_at\": \"$EXPIRES_AT\","
+        echo "  \"size\": \"$(du -h "$TARGET" | awk '{print $1}' 2>/dev/null)\","
+        echo "  \"md5\": \"$(md5sum "$TARGET" | awk '{print $1}' 2>/dev/null)\","
+        echo "  \"sha1\": \"$(sha1sum "$TARGET" | awk '{print $1}' 2>/dev/null)\","
+        echo "  \"sha256\": \"$(sha256sum "$TARGET" | awk '{print $1}' 2>/dev/null)\","
+        echo "  \"sha512\": \"$(sha512sum "$TARGET" | awk '{print $1}' 2>/dev/null)\"",
+        echo "  \"created_at\": \"$(date -Iseconds)\"",
+        echo "  \"binary\": $IS_BINARY"
+        echo "}"
+
     }
     build_file() {
         local FILE=$1
         [[ ! -f "$FILE" ]] && echo "$FILE is not a valid file" | logger error && return 0
         local EXPIRES_AT="${2:-$CONFIG_EXPIRES_AT}"
         local FILENAME && FILENAME=$(basename -- "$FILE") && FILENAME="${FILENAME%.*}"
-        colorize "blue" "Building file ${FILE} valid until ${EXPIRES_AT}" | logger info
+        wysiwyg colorize "blue" "Building file ${FILE} valid until ${EXPIRES_AT}" | logger info
         # shc -r -f "${FILE}" -e "${EXPIRES_AT}"
         # cp -f "$FILE" "$DIST/$FILENAME.sh"          # .s = sh source
         # mv -f "${FILE}.x" "${BIN}/$FILENAME"        # .x = executable
         # mv -f "${FILE}.x.c" "${DIST}/$FILENAME.c"   # .c = c source
         # stats "$FILE" "$EXPIRES_AT"
-        colorize "green" "Build done. run ${BIN}/${FILENAME}" | logger success
+        wysiwyg colorize "green" "Build done. run ${BIN}/${FILENAME}" | logger success
     }
-    _bundle() {
-        local SRC=${1:?}
-        local EXPIRES_AT="${3:-$CONFIG_EXPIRES_AT}"
-        [[ ! -d "$SRC" ]] && echo "${SRC} is not a valid directory" | logger error && return 0
-        local MAIN && MAIN=$(find "$SRC" -type f -name ".rapd.sh" -or -name "main.sh" | head -n 1)
-        [[ ! -f "$MAIN" ]] && echo "Invalid module main file, create a file ${YELLOW}<module>.rapd.sh${NC} in ${TARGET} folder" | logger from_buf error && return 0
-        local BUNDLE_FILENAME && BUNDLE_FILENAME=$(basename -- "$MAIN")
-        local BUNDLE_NAME="${BUNDLE_FILENAME%.*}"
-        [[ "$BUNDLE_NAME" == "main" ]] && BUNDLE_NAME="rapd"
-        [[ "$BUNDLE_NAME" != "rapd" ]] && BUNDLE_NAME="rapd.${BUNDLE_NAME}"
-        local BUNDLE="${DIST}/${BUNDLE_NAME}"
-        [[ -f "$BUNDLE" ]] && rm -f "$BUNDLE"
-        touch "$BUNDLE"
-        local LIB=${SRC}/lib
-        [[ ! -d "$LIB" ]] && echo "${LIB} is not a valid directory" | logger error && return 0
+    copywrite() {
+        echo "# YELLOW TEAM BUNDLE"
+        echo "# $(jq -c .yellowteam <<<"$YWT_CONFIG")"
+        echo "# This file is generated by yellowteam sdk builder. Do not edit this file"
+        echo "# Build date: $(date -Iseconds)"
+        echo "# Build ID: $(git rev-parse HEAD 2>/dev/null || echo "Unknown")"
+        echo "# Build branch: $(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo "Unknown")"
+        echo "# Build tag: $(git describe --tags 2>/dev/null || echo "Unknown")"
+        echo "# Build commit: $(git rev-parse --short HEAD 2>/dev/null || echo "Unknown")"
+        echo "# Build author: $(git log -1 --pretty=format:'%an <%ae>' 2>/dev/null || echo "Unknown")"
+        echo "# Build message: $(git log -1 --pretty=format:'%s' 2>/dev/null || echo "Unknown")"
+    }
+    __bundle() {
+        local SRC_FILE=${1:?} && [ ! -f "$SRC_FILE" ] && echo "Invalid source file" | logger error && return 1
+        local FILE_EXT="${SRC_FILE##*.}" && [[ "$FILE_EXT" != "sh" ]] && echo "Invalid file extension" | logger error && return 1
+        local FILENAME=$(basename -- "$SRC_FILE") && FILENAME="${FILENAME%.*}"
+        local FILE_BASEPATH=$(dirname -- "$SRC_FILE")
+        local FILE_REALPATH=$(realpath -- "$FILE_BASEPATH")
+        local FILE_RELATIVEPATH=$(realpath --relative-to="$SRC_FILE" "$FILE_BASEPATH")
+        local BUNDLE_NAME="${2:-"${FILENAME}.sh"}" && [ -z "$BUNDLE_NAME" ] && echo "Invalid bundle name" | logger error && return 1
+        local EXPIRES_AT="${3:-$CONFIG_EXPIRES_AT}" && [ -z "$EXPIRES_AT" ] && EXPIRES_AT="31/12/2999"
+        local LIB=${FILE_REALPATH}/lib && [[ ! -d "$LIB" ]] && echo "${LIB} is not a valid directory" | logger error && return 0
+        # local SRC=${3:-"${YWT_CONFIG_BUILDER_SRC:-"$(jq -r .path.src <<<"$YWT_CONFIG")"}"}
+        local TARGET_FILE="${SRC}/${BUNDLE_NAME}" && [[ -f "$TARGET_FILE" ]] && rm -f "$TARGET_FILE"
+        # {
+        #     debug -n "{"
+        #     debug -n "\"FILENAME\": \"$FILENAME\","
+        #     debug -n "\"FILE_BASEPATH\": \"$FILE_BASEPATH\","
+        #     debug -n "\"FILE_REALPATH\": \"$FILE_REALPATH\","
+        #     debug -n "\"FILE_RELATIVEPATH\": \"$FILE_RELATIVEPATH\","
+        #     debug -n "\"BUNDLE_NAME\": \"$BUNDLE_NAME\","
+        #     debug -n "\"EXPIRES_AT\": \"$EXPIRES_AT\","
+        #     debug -n "\"LIB\": \"$LIB\","
+        #     debug -n "\"TARGET_FILE\": \"$TARGET_FILE\","
+        #     debug -n "}"
+        # }
         _inject() {
             local FILE="$1"
             [[ ! -f "$FILE" ]] && echo "$FILE is not a valid file" | logger error && return 0
@@ -87,72 +101,48 @@ builder() {
         }
         {
             echo "#!/bin/bash"
-            echo "# YELLOW TEAM BUNDLE"
-            echo "# $(jq -c .yellowteam <<<"$YWT_CONFIG")"
-            echo "# This file is generated by yellowteam sdk builder. Do not edit this file"
-            echo "# Build date: $(date -Iseconds)"
-            echo "# Build ID: $(git rev-parse HEAD)"
-            echo "# Build branch: $(git rev-parse --abbrev-ref HEAD)"
-            echo "# Build tag: $(git describe --tags)"
-            echo "# Build commit: $(git rev-parse --short HEAD)"
-            echo "# Build author: $(git log -1 --pretty=format:'%an <%ae>')"
-            echo "# Build message: $(git log -1 --pretty=format:'%s')"
-            echo "# Build expires at: $CONFIG_EXPIRES_AT"
+            echo "# shellcheck disable=SC2044,SC2155,SC2317"
+            copywrite
             while read -r FILE; do
                 local FILENAME && FILENAME=$(basename -- "$FILE")
                 local RELATIVE_PATH && RELATIVE_PATH=$(realpath --relative-to="$SRC" "$FILE")
                 local RELATIVE_PATH && RELATIVE_PATH=$(dirname -- "$RELATIVE_PATH")
-                echo "Packaging ${RELATIVE_PATH}/${FILENAME}" | logger debug
-                {
-                    echo -e "\n# start of $FILENAME\n" &&
-                        grep -v "^#" <"$FILE"
-                } >>"$BUNDLE"
-                echo -e "\n# end of $FILENAME\n" >>"$BUNDLE"
+                echo -e "# $(_stats "$FILE" "$EXPIRES_AT" | jq -c .)\n"
+                _inject "$FILE"
+                echo -e "\n# end of $FILENAME\n"
             done < <(find "$LIB" -type f -name "*.ywt.sh" | sort)
-            echo "Packaging ${GREEN}${MAIN}${NC}" | logger debug
-            cat "$MAIN" >>"$BUNDLE"
-            echo -e "\nrapd \"\$@\"" >>"$BUNDLE"
-        } >>"$BUNDLE"
-        cat "$BUNDLE"
+            # echo "Packaging ${GREEN}${SRC_FILE}${NC}" | logger debug
+            echo -e "# $(_stats "$SRC_FILE" "$EXPIRES_AT" | jq -c .)\n"
+            _inject "$SRC_FILE"
+            echo -e "\n# end of $SRC_FILE\n"
+            # echo -e "\nif [ \"\$#\" -gt 0 ]; then $FILENAME \"\$@\"; fi"
+        } >>"$TARGET_FILE"
+        _stats "$TARGET_FILE" "$EXPIRES_AT" | jq -Cc . | logger info
+    }
+    __build_file() {
+        local FILE=$1 && readonly FILE && [[ ! -f "$FILE" ]] && echo "$FILE is not a valid file" | logger error && return 0
+        local FILE_DIR=$(dirname -- "$FILE") && readonly FILE_DIR
+        local EXPIRES_AT="${2:-$CONFIG_EXPIRES_AT}"
+        local FILENAME && FILENAME=$(basename -- "$FILE") && FILENAME="${FILENAME%.*}"
+        wysiwyg colorize "blue" "Building file ${FILE} valid until ${EXPIRES_AT}" | logger info
+        shc -r -f "${FILE}" -e "${EXPIRES_AT}"
+        # .s = sh source | .c = c source | .x = executable
+        cp -f "$FILE" "$DIST/$FILENAME.sh"          # /dist/file.sh
+        cp -f "${FILE}.x.c" "$DIST/$FILENAME.c"     # /dist/file.c 
+        mv -f "${FILE}.x" "${BIN}/$FILENAME"        # /bin/file
+        mv -f "${FILE}.x.c" "${FILE_DIR}/$FILENAME.c"           # ./file.c
+        echo '{
+            "sh": "'"$(_stats "$DIST/$FILENAME.sh" "$EXPIRES_AT" | jq .)"'",
+            "c": "'"$(_stats "$BIN/${FILENAME}.c" "$EXPIRES_AT" | jq .)"'",
+            "bin": "'"$(_stats "$BIN/$FILENAME" "$EXPIRES_AT" | jq .)"'",  
+        }' | jq -C . | logger info
+        wysiwyg colorize "green" "Build done. run ${BIN}/${FILENAME}" | logger success
     }
     _build_sdk() {
-        local SDK=$(jq -r .path.sdk <<<"$YWT_CONFIG") && SRC=$(realpath -- "$SDK")
-        _bundle "$SRC" && return 0
-        _inject() {
-            local FILE="$1"
-            [[ ! -f "$FILE" ]] && echo "$FILE is not a valid file" | logger error && return 0
-            grep -v "^#" "$FILE" | grep -v "^$" | grep -v "^#!/usr/bin/env bash"
-        }
-        
-        logger info "${CONTEXT}Building sdk ($SRC)"
         _prepare
-        local SDK_SRC_FILE="${SDK}/sdk.sh"
-        [[ ! -f "$SDK_SRC_FILE" ]] && logger error "${CONTEXT}File $SDK_SRC_FILE not found" && return 1
-        local SDK_LIB=$(jq -r .path.lib <<<"$YWT_CONFIG") && readonly SDK_LIB
-        local SDK_DIST_FILE="${DIST}/sdk.sh"
-        [[ -f "$SDK_DIST_FILE" ]] && rm -f "$SDK_DIST_FILE"
-        {
-            echo "#!/usr/bin/env bash"
-            echo "# YELLOW TEAM SDK"
-            echo "# $(jq -c .yellowteam <<<"$YWT_CONFIG")"
-            echo "# This file is generated by yellowteam sdk builder. Do not edit this file"
-            echo "# Build date: $(date -Iseconds)"
-            echo "# Build ID: $(git rev-parse HEAD)"
-            echo "# Build branch: $(git rev-parse --abbrev-ref HEAD)"
-            echo "# Build tag: $(git describe --tags)"
-            echo "# Build commit: $(git rev-parse --short HEAD)"
-            echo "# Build author: $(git log -1 --pretty=format:'%an <%ae>')"
-            echo "# Build message: $(git log -1 --pretty=format:'%s')"
-            echo "# Build expires at: $CONFIG_EXPIRES_AT"
-            while read -r FILE; do
-                local FILE_NAME && FILE_NAME=$(basename -- "$FILE") && FILE_NAME="${FILE_NAME%.*}" && FILE_NAME=$(echo "$FILE_NAME" | tr '[:upper:]' '[:lower:]')
-                [ ! -f "$FILE" ] && continue
-                # shellcheck source=/dev/null # echo "source $FILE" 1>&2 &&
-                _inject "$FILE"
-            done < <(find "$SDK_LIB" -type f -name "*.ywt.sh" | sort)
-            _inject "$SDK_SRC_FILE"
-        } >>"$SDK_DIST_FILE"
-        cat "$SDK_DIST_FILE"
+        __bundle "$SDK/sdk.sh" "ywt.sh" "31/12/1999"
+        __build_file "$SRC/ywt.sh" "31/12/1999"
+        return 0
     }
     inspect() {
         jq -r '.path' <<<"$YWT_CONFIG"
